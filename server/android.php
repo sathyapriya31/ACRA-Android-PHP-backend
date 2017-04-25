644f144dfcec79e80984065858705a12afeeacd0
Gamers Cave <admin@gamers-cave-world.com>
2017-04-25 17:59:29 +0200

    Removed incorrect information

--- server/android.php
+++ server/android.php
@@ -3,7 +3,7 @@ include "creds.php";
 
 if (isset($_POST['username']) && isset($_POST['password'])) {
     if($_POST['username'] == $USERNAME && $_POST['password'] == $PASSWORD){
-        //Defines the extension we use. No period before it, just the raw name
+        //Defines the extension we use. 
         $extension = ".txt";
         
         $files = glob("logs/*" . $extension);
